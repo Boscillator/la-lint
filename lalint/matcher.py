ec3a9e4d6d50d9131d117772db4eeed0fb745eab
Fred Buchanan <oscillator.b@gmail.com>
2018-06-11 15:28:30 -0400

    Refactored to base matcher class

--- lalint/matcher.py
+++ lalint/matcher.py
@@ -1,19 +1,17 @@
 import re
+import abc
 
 from .tokenizer.tokens import *
 
-class RegexMatcher(object):
-
-    def __init__(self, pattern):
-        self.pattern = pattern
+class Matcher(abc.ABC):
 
     def match(self, document):
         out = []
         for token in document.body:
-            if isinstance(token, Text):
-                if re.match(self.pattern, token.value):
-                    out.append(token)
-            elif isinstance(token, Command):
+            if self.token_matches(token):
+                out.append(token)
+            
+            if isinstance(token, Command):
                 for arg in token.args:
                     out += self.match(arg)
             elif isinstance(token, Math):
@@ -21,29 +19,36 @@ class RegexMatcher(object):
 
         return out
 
-class CommandMatcher(object):
+    @abc.abstractmethod
+    def token_matches(self, token):
+        """
+        Returns true if the token should match this matcher.
+        """
+        pass
+
+class RegexMatcher(Matcher):
+
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    def token_matches(self, token):
+        return isinstance(token,Text) and re.match(self.pattern, token.value)
+
+class CommandMatcher(Matcher):
 
     def __init__(self,name, args = []):
         self.name = name
         self.args = args
 
-    def match(self, document):
-        out = []
-        for token in document.body:
-            if isinstance(token, Command):
-                if token.name == self.name:
-                    all_args_match = True
-                    for matcher, argument in zip(self.args, token.args):
-                        if len(matcher.match(argument)) == 0:
-                            all_args_match = False
+    def token_matches(self, token):
+        if isinstance(token, Command):
+            if token.name == self.name:
+                all_args_match = True
+                for matcher, argument in zip(self.args, token.args):
+                    if len(matcher.match(argument)) == 0:
+                        all_args_match = False
 
-                    if all_args_match:
-                        out.append(token)
+                if all_args_match:
+                    return True
                 
-                for arg in token.args:
-                    out += self.match(arg)
-
-            elif isinstance(token, Math):
-                out += self.match(token.body)
-                
-        return out
+        return False
