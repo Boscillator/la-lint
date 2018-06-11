e1ad7c088d65e0408cc95b093115c7e78147704c
Fred Buchanan <oscillator.b@gmail.com>
2018-06-11 14:09:47 -0400

    Documented tokenize

--- lalint/tokenizer.py
+++ lalint/tokenizer.py
@@ -119,5 +119,16 @@ document << Group(ZeroOrMore( command | text | inlineMath | displayedMath )).set
 document.setParseAction(lambda t: Document(t.body.asList()))
 
 def tokenize(source_str):
+    """
+    Converts a latex document into a Document
+    Parameters
+    ----------
+    source_str : str
+        A string to convert into a document
+    Returns
+    -------
+    Document
+        A document representing source_str
+    """
     res = document.parseString(source_str)
     return res.asList()[0]
