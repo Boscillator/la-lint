f94c98bc108d2ee71945819ea184f3e4833b5eab
Fred Buchanan <oscillator.b@gmail.com>
2018-06-11 13:43:09 -0400

    Refactored to use Document class

--- lalint/tokenizer.py
+++ lalint/tokenizer.py
@@ -1,9 +1,27 @@
 from collections import namedtuple
-from typing import List, Any
 from pyparsing import *
 
 # Command = namedtuple('Command',['name','args'])
 # Command.__new__.__defaults__ = ([],)
+
+class Document(object):
+    """Repersents a selection of LaTeX"""
+
+    def __init__(self, body):
+        self.body = body
+
+    def __repr__(self):
+        return f"<Document body='{self.body}'>"
+
+    def __eq__(self, other):
+        print("Document EQ")
+        if type(other) == type(self):
+            return self.body == other.body
+        elif type(other) == list:
+            print("using list comparison")
+            return self.body == other
+        else:
+            return False
 
 class Command(object):
     r"""
@@ -13,7 +31,7 @@ class Command(object):
     ----------
     name : str
         The text that comes directly after the backslash
-    args : List[List[Union[str,Command,Math]]], optional
+    args : List[Document], optional
         The list of args to the command. Each argument is a list of strings and Commands and Maths which represent the body of that command.
     """
 
@@ -48,7 +66,7 @@ bracket_arg.setParseAction(lambda t: t.body)
 
 argument = brace_arg | bracket_arg
 
-arg_list = ZeroOrMore(Group(argument))
+arg_list = ZeroOrMore(argument)
 #arg_list.setParseAction(lambda t: print(t))
 
 def handelCommand(t):
@@ -64,13 +82,14 @@ command.setParseAction(handelCommand)
 
 dollar = Literal("$")
 inlineMath = dollar + document.setResultsName("body") + dollar
-inlineMath.setParseAction(lambda t: Math(t.body.asList()))
+inlineMath.setParseAction(lambda t: Math(t.body))
 
 special_char = slash | lbrace | rbrace | lbracket | rbracket | dollar
 word = ~special_char + Regex(r'[\s\S]')
 text = Combine(OneOrMore(word))
 
-document << Group(ZeroOrMore( command | text | inlineMath))
+document << Group(ZeroOrMore( command | text | inlineMath)).setResultsName('body')
+document.setParseAction(lambda t: Document(t.body.asList()))
 
 def tokenize(source_str):
     res = document.parseString(source_str)
